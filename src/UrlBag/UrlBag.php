6bf27767cfd1560e586f46968f4f4bc52b8a8dc9
troybeckette <troybeckette@gmail.com>
2016-10-21 00:14:57 +0200

    urlBag updated

--- src/UrlBag/UrlBag.php
+++ src/UrlBag/UrlBag.php
@@ -86,6 +86,7 @@ class UrlBag implements UrlBagInterface
     private function initializeBag()
     {
         $scriptName = '';
+        $dirname = '/';
         $rewriteOn = false;
 
         if (php_sapi_name() !== 'cli') {
@@ -99,17 +100,21 @@ class UrlBag implements UrlBagInterface
 
         $context = (!empty($info['basename']) && php_sapi_name() !== 'cli' && false === $rewriteOn) ? "/{$info['basename']}" : '';
 
-        /* Normalize windows and *nix directory name by using "/" */
-        /* ex: windows directory name can be "\" and *nix directory name is "/" */
-        $dirname = str_replace("\\", '/', $info['dirname']);
-
         $scheme = !empty($parseUrl['scheme']) ? "{$parseUrl['scheme']}" : '';
         $host = !empty($parseUrl['host']) ? "{$parseUrl['host']}" : '';
         $port = !empty($parseUrl['port']) ? ":{$parseUrl['port']}" : '';
         $query = !empty($parseUrl['query']) ? "?{$parseUrl['query']}" : '';
 
         $this->path = !empty($parseUrl['path']) ? "{$parseUrl['path']}{$query}" : '';
-        $this->assetPath = (!empty($dirname) && $dirname != '/') ? $dirname . '/' : '/';
+
+        /* Normalize windows and *nix directory name by using "/" */
+        /* Ex: windows directory name can be "\" and *nix directory name is "/" */
+        /* This changes all "\" to "/" */
+        if (!empty($info['dirname'])) {
+            $dirname = str_replace("\\", '/', $info['dirname']);
+        }
+
+        $this->assetPath = $dirname != '/' ? $dirname . '/' : $dirname;
 
         $this->basePath = rtrim($this->assetPath, '/') . $context;
         $this->baseUrl = "{$scheme}://{$host}{$port}{$this->basePath}";
